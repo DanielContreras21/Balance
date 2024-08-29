0463ef4e61e55db56cda7a2c318c3901ab8c1d55
Dcopito <danielcp62.dc@gmail.com>
2024-08-29 00:38:45 -0500

    cambios

---
+++ Frontend/scripts/utils.js
@@ -0,0 +1,27 @@
+const PRODUCTION = false
+function backendUrl(){
+    if(PRODUCTION){
+        return "https://balance-tests.up.railway.app"
+    } else{
+        return "http://localhost:8080"
+    }
+}
+
+const JWT = localStorage.getItem('jwt')
+
+const NUMBERFORMATTER = new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+
+const DATEFORMATTER = new Intl.DateTimeFormat('es-CO', {
+    year:'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+});
--- Frontend/urls.js
+++
@@ -1,8 +0,0 @@
-const PRODUCTION = false
-function bacekendUrl(){
-    if(PRODUCTION){
-        return "https://balance-tests.up.railway.app"
-    } else{
-        return "http://localhost:8080"
-    }
-}

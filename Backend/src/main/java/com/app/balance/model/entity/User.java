69ef713de47c9b53ca5e44b1a360489f9931b43f
Dcopito <danielcp62.dc@gmail.com>
2024-08-29 23:07:40 -0500

    termino la maquetación

--- Backend/src/main/java/com/app/balance/model/entity/User.java
+++ Backend/src/main/java/com/app/balance/model/entity/User.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-
 @Data
 @Builder
 @AllArgsConstructor
--- Frontend/scripts/utils.js
+++ Frontend/scripts/utils.js
@@ -16,3 +16,26 @@ const DATEFORMATTER = new Intl.DateTimeFormat('es-CO', {
     second: '2-digit',
     hour12: false
 });
+
+const openModal = (modalId) =>{
+    const modal = document.getElementById(modalId)
+    modal.showModal()
+}
+
+const closeModal = (modalId) =>{    
+    const modal = document.getElementById(modalId)
+    modal.close()
+}
+
+function formatDateToString(dateInMilliseconds) {
+    const date = new Date(dateInMilliseconds);
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}-${month}-${year} ${hours}:${minutes}`;
+}
